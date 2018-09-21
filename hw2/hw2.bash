e1350036ba854c780d43374beec5339d3749c8f6
Fan Zhang <zhangfan.v@outlook.com>
2018-09-20 17:22:20 -0700

    Updated .bash script

--- hw2/hw2.bash
+++ hw2/hw2.bash
@@ -9,6 +9,12 @@ python train_pg_f18.py CartPole-v0 -n 100 -b 1000 -e 3 -rtg -dna --exp_name sb_r
 
 python train_pg_f18.py CartPole-v0 -n 100 -b 1000 -e 3 -rtg --exp_name sb_rtg_na
 
+python train_pg_f18.py CartPole-v0 -n 100 -b 5000 -e 3 -dna --exp_name lb_no_rtg_dna
+
+python train_pg_f18.py CartPole-v0 -n 100 -b 5000 -e 3 -rtg -dna --exp_name lb_rtg_dna
+
+python train_pg_f18.py CartPole-v0 -n 100 -b 5000 -e 3 -rtg --exp_name lb_rtg_na
+
 #Peoblem 5
 
 python train_pg_f18.py InvertedPendulum-v2 -ep 1000 --discount 0.9 -n 100 -e 3 -l 2 -s 64 -b 400 -lr 0.02 -rtg --exp_name hc_b400_r0.02
